0a17cfca3a3bc2c88560c4934714231b9a752435
Lee Jae Hyu <wogb0918@gmail.com>
2025-04-02 15:35:28 +0900

    [Feat] 버튼 추가
    타이틀 화면에서 종료 버튼 누르면 게임 종료

--- Assets/Scripts/Scene/SceneChanger.cs
+++ Assets/Scripts/Scene/SceneChanger.cs
@@ -31,6 +31,15 @@ public class SceneChanger : MonoBehaviour
         //StartCoroutine(ChangeSceneAfterDelay());
     }
 
+    public void GameExit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false; // 유니티 에디터에서는 플레이 모드 종료
+#else
+        Application.Quit(); // 빌드된 게임에서는 게임 종료
+#endif
+    }
+
     //IEnumerator ChangeSceneAfterDelay()
     //{
     //    Debug.Log("ChangeScene");
