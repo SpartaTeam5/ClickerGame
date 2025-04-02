66e509a96f26d6004d86374751ca0f5228f5d227
Kim Tae-gyeom <114921436+BeautifulMaple@users.noreply.github.com>
2025-04-02 11:28:24 +0900

    [ Test ] 엔딩 씬 -> 타이틀 씬 테스트 중

--- Assets/Scripts/Scene/SceneChanger.cs
+++ Assets/Scripts/Scene/SceneChanger.cs
@@ -8,18 +8,23 @@ public class SceneChanger : MonoBehaviour
     public float delayTime = 20f; // 전환 대기 시간 (초)
     public string nextSceneName; // 전환할 씬 이름
 
-    void Start()
-    {
-        StartCoroutine(ChangeSceneAfterDelay());
-    }
-    public void SceneChange(string sceneName)
+    //void Start()
+    //{
+    //    StartCoroutine(ChangeSceneAfterDelay());
+    //}
+    public void SceneChange(string sceneName)   // 게임 시작 씬
     {
         SceneManager.LoadScene("Testscenes");
     }
 
-    public void ExitGame(string sceneName)
+    public void ExitGame(string sceneName)  // 게임 종료
     {
         SceneManager.LoadScene("TitleScene");
+    }
+    public void EndSceneChanage()   // 엔딩
+    {
+        SceneManager.LoadScene("EndingScene");
+        StartCoroutine(ChangeSceneAfterDelay());
     }
 
     IEnumerator ChangeSceneAfterDelay()
