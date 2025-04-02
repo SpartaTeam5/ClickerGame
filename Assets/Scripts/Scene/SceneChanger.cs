45ce9b484f0ca5b78efef7d2c2716b8a22bf0b50
Lee Jae Hyu <wogb0918@gmail.com>
2025-04-02 11:21:02 +0900

    [Feat] 엔딩씬 코드 추가
    엔딩 씬에서 일정 시간 경과하면 타이틀 씬으로 이동

--- Assets/Scripts/Scene/SceneChanger.cs
+++ Assets/Scripts/Scene/SceneChanger.cs
@@ -5,6 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class SceneChanger : MonoBehaviour
 {
+    public float delayTime = 20f; // 전환 대기 시간 (초)
+    public string nextSceneName; // 전환할 씬 이름
+
+    void Start()
+    {
+        StartCoroutine(ChangeSceneAfterDelay());
+    }
     public void SceneChange(string sceneName)
     {
         SceneManager.LoadScene("Testscenes");
@@ -12,6 +19,12 @@ public class SceneChanger : MonoBehaviour
 
     public void ExitGame(string sceneName)
     {
+        SceneManager.LoadScene("TitleScene");
+    }
+
+    IEnumerator ChangeSceneAfterDelay()
+    {
+        yield return new WaitForSeconds(delayTime);
         SceneManager.LoadScene("TitleScene");
     }
 }
